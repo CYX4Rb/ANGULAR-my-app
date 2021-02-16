246b09957176f079590f9395cbdc6cd605c1c350
CYX4Rb <70616505+CYX4Rb@users.noreply.github.com>
2021-02-17 02:44:15 +0300

    card-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { CardComponent } from './card/card.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
